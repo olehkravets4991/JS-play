a917105847b27628c63cb70a9b11f24e8492dda4
Oleh <kravetsoleh4991@gmail.com>
2023-10-19 23:41:09 -0700

    JS play

---
+++ first.js
@@ -0,0 +1,138 @@
+// //console log a string, number, boolean, null and undefined
+// console.log('hello world')
+// console.log(123)
+// console.log(false)
+// console.log(null)
+// console.log(undefined)
+
+// let obj = {
+//     animal: 'dog',
+//     name: 'spike',
+//     age: 1,
+//     abilities: ['bubble blast', 'lickem','tackle'],
+//     ishuman: false, 
+// }
+
+// console.log(
+//     obj.animal,
+//     obj['animal'],
+// )
+
+// let arr = [1, 2, 3, 4, 5]
+
+// let arrTwo = [
+//     100, 
+//     'hello world',
+//     false,
+//     null, 
+//     { name: 'spike'}
+// ]
+
+// console.log(arrTwo[1])
+
+
+// // Operators
+
+// // + addition +
+// console.log(1 + 1);
+
+// // - subtraction -
+// console.log(1 -1);
+
+// // Multiplication or x *
+// console.log(2 * 2)
+
+// // look up PEMDAS or order of operations
+
+// // Exponentiation **
+// console.log(2 ** 3)
+
+// // Division /
+// console.log(6 / 4)
+
+// // Modulus %
+// // Division remainder
+// console.log(5 % 3)
+
+// // Increment ++
+// let x = 101;
+// x++
+// console.log(x)
+
+// // Decrement --
+// let y = 101;
+// y--
+// console.log(y)
+
+
+// // Increment by value
+// let z = 101;
+// z+=10
+// console.log(z)
+
+// // Decrement by value -=
+// let aa = 101;
+// aa-=10
+// console.log(aa)
+
+// methods
+
+
+// // Math.ceil() function returns the smallest integer greater than or equal to a given number. 
+// console.log(Math.ceil(4.1))
+// console.log(Math.ceil(-4.1))
+
+
+// // Math.max() returns the largest of the numbers.
+// console.log(Math.max(-Infinity, 1, 2, 1000, 0))
+
+
+// // Math.min() returns the smallest of the numbers.
+// console.log(Math.min(-Infinity, 1, 2, 1000, 0))
+
+
+// // Math.random() return a value in between 0 and 1
+// console.log(Math.random());
+
+// // Math.floor() return the largest interger greater than  or queal to give number (sworn nemisis of Math.ceil())
+// console.log(Math.floor(4.1)) //4
+// console.log(Math.floor(-4.1)) //-5
+
+
+// // Math.round() return the value of the next closest interger
+// console.log(Math.round(4.1)) //4
+// console.log(Math.round(-4.1)) //-4
+// console.log(Math.round(4.6)) //4
+// console.log(Math.round(-4.6)) //-4
+
+
+// = one equal means im going to set it to that value afterwards
+// let name = 'David'
+
+
+// === checks for equal value and equal type
+console.log(1 === 1)
+
+// == checks for equal value 
+console.log("1" == 1)
+
+
+// > greater then check
+// >= "greater than or equal" to check
+console.log(1 > 1) 
+console.log(1 >= 1)
+
+// < less then check
+// <= "less than or equal" to check
+console.log(-1 < -1) 
+console.log(-1 <= -1)
+
+// !== check for NON equality (for type and value)
+console.log(1 !== 1)
+console.log(1 !== 2)
+
+// != check for NON equality (for value)
+console.log(1 != 1)
+console.log(1 != 2)
+console.log('1' != 1)
+console.log('1' != 2)
