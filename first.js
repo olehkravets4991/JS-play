12d98abb3257e9a97d13aaa56f490494f4731f11
Kravets Oleh <kravetsoleh4991@gmail.com>
2023-10-27 23:16:35 -0700

    js

--- first.js
+++ first.js
@@ -143,8 +143,491 @@
 // console.log('1' != 1)
 // console.log('1' != 2)
 
-console.log("Oleh")
-console.log("29")
-console.log("03/29")
-console.log("1994")
-console.log("Kravets")
+// console.log('Oleh')
+// console.log(29)
+// console.log(true)
+// console.log(null)
+// console.log(undefined)
+
+// console.log('hello world', 123, false, null, undefined)
+// let a = 'hello world'
+// let b = 123
+// let c = false
+// let d = null
+// let e = undefined
+// console.log(a,b,c,d,e)
+
+
+// let z;
+// let y ='y'
+// let x = 1
+
+// //Object 
+// let obj = {
+//     animal: 'dog',
+//     name: 'spike',
+//     age: 1,
+//     abilities: ['bubble blast', "lickem"],
+//     ishuman: false,
+
+
+// }
+
+// console.log(
+//     obj.animal,
+//     obj['animal']
+// )
+
+// //An array  (starts at 0)
+// let arr =[1,2,3,4,5]
+
+// let arrTwo = [
+//     100,
+//     'hello world',
+//     false,
+//     null,
+//     {name: 'spike'},
+// ]
+
+// console.log(arrTwo[1])
+
+// const oleh = 'oleh';
+
+// let david = 'david1'
+// david ='david2'
+
+// //var was funtion scope
+// //let and const block scope
+
+// console.log(typeof '1')
+// console.log(typeof 1)
+
+
+// //Operators and Methods
+
+// //+addition
+// console.log(1+1);
+
+// //- substruction
+// console.log(1-1);
+
+// //Multiplication
+// console.log(2*2)
+
+// //pendas = order of operations
+
+// //exponentiation
+// console.log(2**3)
+
+// //devision
+// console.log(6/4)
+
+// //Modulus %
+// // Division remaider
+
+// console.log(5 % 3)
+
+// //Increment ++
+// let x = 101;
+// x++
+// console.log( x)
+
+// //Decrement --
+// let y = 101;
+// y--
+// console.log(y)
+
+// //increment by value
+// let z = 101;
+// z+=10
+// console.log(z)
+
+// //Derrement by value
+// let aa = 101;
+// aa-=10
+// console.log(aa)
+
+// //Methods
+// //Math.ceil()
+
+// console.log(Math.ceil(4.1))
+// console.log(Math.ceil(-4.1))
+
+// //Math.max (largest number)
+// console.log(Math.max(1,2,3,1000))
+
+// //Math.min (smallest)
+// console.log(Math.min(-Infinity, 1000))
+
+// //Math.random (between 0 and 1)
+// console.log(Math.random())
+
+// //Math.floor( largest grater or equal to number)
+// console.log(Math.floor(4.1)) //4
+// console.log(Math.floor(-4.1)) //-4
+
+// // Math.round() return the value of the next closest interger
+// console.log(Math.round(4.1)) //4
+// console.log(Math.round(-4.1)) //-4
+// console.log(Math.round(4.6)) //4
+// console.log(Math.round(-4.6)) //-4
+
+
+
+// //= one equal set value
+// // let me = 'Oleh'
+
+// //=== equal value and equal type
+// console.log(1===1)
+
+// //== equal value
+// console.log("1"==1)
+
+
+// //>
+// //>=
+// console.log(1 >1)
+// console.log(1>=1)
+
+// //<
+// //<=
+// console.log(-1<-1)
+// console.log(-1<= -1)
+
+// //!== check for NON equality
+// console.log(1 !== 1)
+// console.log(1 !== 2)
+
+// console.log('1' != 1)
+// console.log('1' != 2)
+
+// console.log(1 === 1)
+// console.log('beans' === 'Beans')
+// console.log((5+5-3) === ((5+5)-3))
+// console.log(9 === 'False')
+// console.log('A' > 'a')
+// console.log('Burger King' < 'McDonalds') 
+// console.log(NaN === NaN )
+
+// //  && and 
+// //true and false -> all true
+// //  || or
+// //true or false -> 1 needs to be true
+
+// console.log(
+//     true && true && true && false
+// )
+
+// console.log( false || false || false || true)
+
+
+
+// console.log(
+//     true || true || true && false
+// )
+
+
+
+// //Truthy and falsey
+
+// //falsey is this value ( 0, false, '', NaN, null, undefined)
+// //truthy is everything thats not a falsey
+
+// console.log(true || false)
+// console.log(null || 'string')
+
+
+// // !! bang 
+// console.log(!!null || !!'string')
+// console.log(false||true)
+
+
+
+
+
+// //if(){
+
+
+// //}else(){
+
+// //}else {
+
+// //}
+
+// // 'if-else' condition
+// // looks for the very first match and runs the code inside of that block
+// // else if are add on's. Kind of like the first if looks for a condition on the else if
+// // else is a catch. 
+// // else if and else are optional in a 'if-else' condition block
+// let isMMColorGreen = 'false';
+
+// if(isMMColorGreen){
+//     console.log('yes')
+// }else {
+//     console.log('no')
+// }
+
+// if(false){
+//     console.log('frist block')
+// }else if(0){
+//     console.log('2nd block')
+// }else {
+//     console.log('3rd block')
+// }
+
+
+// // 4 % 2 --> 0 
+// // 5 % 3 --> 2
+
+// if(5 % 3 === 2){
+//     console.log('i know math')
+// }
+
+// function oleh(){
+
+//     var x = 'test';
+//     {
+//         const block = 'block'
+//         x = 'oleh'
+//         console.log(x)
+//     }
+
+//     console.log(x)
+//     console.log(block) //remove this and then exctue 
+
+
+// }
+// oleh();
+
+// function oleh(){
+
+//     var x = 'test';
+//     {
+//         var block = 'block'
+//         x = 'oleh'
+//         console.log(x)
+//     }
+
+//     console.log(x)
+//     console.log(block)
+
+
+// }
+// oleh();
+
+// //basic for loop
+
+
+// for(let i=0; i<10; i++){
+//     console.log(i)
+// }
+
+// //starting at 0
+// // going to 9
+// //journey from 0 to 9
+// // console.log(i)
+
+// //////////////
+
+// for(let i=0; i<=10; i++){
+//     console.log('going to annoy',i, 'time(s)');
+// }
+
+// for(let i=1; i<=10; i++){
+//     console.log('I did it',i, 'time(s)')
+// }
+
+// // ${} is a string
+
+// //////////////////
+
+// for(i=0; i<11; i++){
+//     console.log(i,i % 2 === 0 )
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+
+// let count = 0;
+// for(let i=0; i<=10; i++){
+//     //if the i is EVEN
+//     if(i % 2 === 0){
+//         // add i to the current value of count
+//         count = count + i   
+//     }
+// }
+
+// console.log(count)
+
+
+// let countTwo = 0;
+// for(let i=0; i<=10; i++){
+//     // add i to the current value of countTwo
+//     countTwo = countTwo + i   
+// }
+// console.log(countTwo)
+
+// // examples of arrays
+// const arr = [];
+// const squares = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+// const satchel = [
+//     'chair', //0
+//     'table', //1
+//     'candle', //2
+//     'map', //3
+//     'magnifying glass', //4
+//     'rupees', //5
+//     'Pepto Bismol', //6
+//     'boomerang' //7
+// ];
+
+// //Accessing element
+// //we can hard code for the element to be accessed
+// console.log(satchel[5]) // rupees
+// console.log(satchel[100]) // undefined because no index of 100
+
+// //we can use a variable for the element to be accessed
+// const x = 2 * 2 + 1 //5
+// console.log(satchel[x]) //same as console.log(satchel[5])
+
+
+// // .length gives the length of the array
+// console.log(satchel.length)
+
+// // if i want to log the last element on the array
+// //here are three ways
+// console.log(satchel[7])
+// console.log(satchel[satchel.length - 1])
+
+// let boom = satchel.length - 1
+// console.log(satchel[boom])
+
+// //if we want to get the middle
+// console.log(satchel[satchel.length/2])
+// const shortArray = ['frist', 'second', 'third'];
+
+// console.log(shortArray[Math.floor(shortArray.length / 2)])
+// console.log(satchel[Math.floor(satchel.length / 2)])
+
+
+// //===========================================================
+
+// //iterating through an array using a for loop and the LENGTH of the array
+// // THIS IS IMPORTANT!
+
+// for(let i=0;i<satchel.length;i++){
+//     console.log(satchel[i])
+// }
+
+
+// //===========================================================
+
+// const pairs = [
+//     ['Harold', 'Kumar'],
+//     ['Laurel', 'Hardy']
+// ];
+
+// console.log(pairs[0][1]) //KUMAR
+
+
+
+// THESE METHODS CAN ONLY BE USED AGAINST ARRAY
+
+//const beasts = ['ant', 'bison', 'bison', 'camel', 'duck', 'bison'];
+//console.log(beasts)
+// indexOf
+// return the first index at which the match presents itself
+
+// console.log(
+//     beasts.indexOf('bison') //1
+// )
+
+// console.log(
+//     beasts.indexOf('mbison') //-1 (bvecause it doesnt exist in the array)
+// )
+
+//========================================
+
+// push
+// the push method adds to the END of an array
+// MUTATION
+// beasts.push('cow')
+// console.log(beasts)
+
+
+//========================================
+
+// pop
+// the pop methods removes the last element from the array
+// MUTATION
+// beasts.pop()
+// console.log(beasts)
+
+
+
+//========================================
+
+// reverse
+//the revsere method resrve an array in place. 
+// the frist becomes the last and so forth
+// MUTATION
+// beasts.reverse()
+// console.log(beasts)
+
+//========================================
+
+// unshift
+// the unshift adds the specifed menets to the beginning of an array
+// MUTATION
+// beasts.unshift('dog', 'cat', 'hamster')
+// console.log(beasts)
+
+//========================================
+
+// shift 
+// shift removes the first element in the array
+// MUTATION
+// beasts.shift()
+// console.log(beasts)
+
+//========================================
+
+// splice
+// splice method chagnes the content of any array by removing OR REPLACING elements
+// MUTATION
+// beasts.splice(3, 0, 'cow')
+// console.log(beasts)
+
+//========================================
+
+// sort 
+// sort using ASCII by default
+// MUTATION
+//advanced 
+// BUT if you pass in a call back we can make things sort based on that call back function
+// const months = ['march', 'March', 'jan', 'Jan', 'Feb', 'Dec'];
+// months.sort();
+// console.log(months);
+
+
+// const arrayOne = [1, 30, 4, 21, 100000];
+// arrayOne.sort();
+// console.log(arrayOne);
+
+
+
+// const arrayTwo = [1, 30, 4, 21, 100000];
+// arrayTwo.sort((a,b) => a - b );
+
+// console.log(arrayTwo)
+
+//========================================
+
+// slice
+// return a shallow copy of a portion of an array specified from the start and end (not inclusive of the end)
+
+// const beastsTwo = beasts.slice(2,4);
+// console.log(beasts)
+// console.log(beastsTwo)
+
